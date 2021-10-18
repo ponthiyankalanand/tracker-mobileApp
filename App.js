481a665cf89e04b10caccbcba36ec212917f0c68
ponthiyankalanand <79076366+ponthiyankalanand@users.noreply.github.com>
2021-10-19 00:58:13 +0530

    login Layout
    basic login layout done

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+	const [userName, setName] = useState();
+	const [Password, setPassword] = useState();
+	const [serviceID, setserviceID] = useState();
+
+	return(
+		<View style={styles.container}>
+			<Text style={styles.boldText}>Sync Account</Text>
+			<View style={styles.buttonContainer}>
+			<TextInput style={styles.input} placeholder='User Name'
+			onChangeText={(val) => setName(val)}/>
+			<TextInput style={styles.input} placeholder= 'Password'
+			onChangeText={(val) => setPassword(val)}/>
+			<TextInput style={styles.input} placeholder= 'Service ID'
+			onChangeText={(val) => setserviceID(val)}/>
+
+				
+			<Button title='Sync' />
+			</View>
+		</View>
+
+		);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container:{
+		alignItems: 'center',
+		justifyContent:'center',
+		padding:20,
+		backgroundColor: 'lavender',
+		width: 300
+
+	},
+	boldText:{
+		fontWeight: 'bold',
+	},
+	buttonContainer:{
+		marginTop:20
+	},
+	input:{
+		boaderWidth: 1,
+		borderColor: '#777',
+		padding: 20,
+		margin:10
+
+
+	}
+
+})
