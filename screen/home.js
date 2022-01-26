c5c7594b8eff4225c0a1f594fe1a2a98a74ff9b2
ponthiyankalanand <79076366+ponthiyankalanand@users.noreply.github.com>
2022-01-26 17:41:55 +0530

    background image added
    app background image aaded.

--- screen/home.js
+++ screen/home.js
@@ -1,38 +1,39 @@
 import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
+import { ImageBackground,Platform, Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import * as Location from 'expo-location';
-
 export default function home({ navigation, route }){
 	const token  = navigation.getParam('token');
   const userid  = navigation.getParam('userID');
 	//const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-  let location='';
-  let message='';
-   
+  const [message, setmessage] = useState();
+  let loc='';
+  let location='';   
   async function sendLocation() {
-
-    if (Platform.OS === 'android' && !Constants.isDevice) {
-        setErrorMsg(
-          'Oops, this will not work on Snack in an Android emulator. Try it on your device!'
-        );
-        return;
-      }
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      let loc = await Location.getCurrentPositionAsync({});
-
+    try{
+      if (Platform.OS === 'android' && !Constants.isDevice) {
+          setErrorMsg(
+            'Oops, this will not work on Snack in an Android emulator. Try it on your device!'
+          );
+          return;
+        }
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
+        loc = await Location.getCurrentPositionAsync({});
+    }
+    catch{
+      console.log('fetch location failed');
+    }
     console.log(loc.coords.latitude);
       try {
         const response = await fetch('http://127.0.0.1:8000/updateLocation',{
             method: 'post',
              headers: {
-                Accept: 'application/json',
+              Accept: 'application/json',
               'Content-Type': 'application/json',
               'Authorization': 'token ' + token
             },
@@ -44,33 +45,30 @@ export default function home({ navigation, route }){
         })
         })
           const json = await response.json(); 
-          if (json.status == "001")
-          {     
-            console.log("location update Success") 
+          if (json.status == "001"){     
+              console.log("location update Success") 
+              setmessage('location updated');
           }
           else{
             console.log("failed")
             setmessage('location update failed')
              
-          }
-          
+          }          
           
       }
       catch (error) {
           console.error(error);
       } 
-
     }
-
-sendLocation();
-
+  sendLocation();
   return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>{message}</Text>
-    </View>
+    <ImageBackground source={require('../assets/background.png')} style={styles.image} >
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>{message}</Text>
+      </View>
+    </ImageBackground>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -81,5 +79,11 @@ const styles = StyleSheet.create({
   paragraph: {
     fontSize: 18,
     textAlign: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    weight:'100%',
+    heiht:'100%'
   },
 });
--- screen/login.js
+++ screen/login.js
@@ -1,23 +1,16 @@
 import React, { useState} from 'react';
-import { StyleSheet,Button, View, SafeAreaView, Text, Alert, TextInput } from 'react-native';
-
-
+import { ImageBackground,StyleSheet,Button, View, SafeAreaView, Text, Alert, TextInput } from 'react-native';
 export default function login( { navigation } ){
 	const [userName, setName] = useState();
 	const [Password, setPassword] = useState();
 	const [serviceID, setserviceID] = useState();
 	const [data, setData] = useState();
 	const [message, setmessage] = useState();
-
-
 	const pressHandler = (val1,val2) =>{
 		navigation.navigate('Home', {token:val1,userID:val2} );
 	}
-
 /* get request for fetch token */
 	const [isLoading, setLoading] = useState(true);
-  	
-
   	const fetchtoken = async () => {
     	try {
     		const response = await fetch('http://127.0.0.1:8000/token',{
@@ -37,11 +30,8 @@ export default function login( { navigation } ){
       		}
       		else{
       			console.log("failed")
-      			setmessage('Username or Passowrd wrong')
-      			 
-      		}
-      		
-      		
+      			setmessage('Username or Passowrd wrong');      			 
+      		}      		
     	}
     	catch (error) {
       		console.error(error);
@@ -49,53 +39,49 @@ export default function login( { navigation } ){
     	finally {
       		setLoading(false);
    		}
-
   	}
-
- 
-
-
 	return(
-		<View style={styles.container}>     	    
-			<Text style={styles.boldText}>Sync Account</Text>
-			<View style={styles.buttonContainer}>
-			<TextInput style={styles.input} placeholder='User Name'
-			onChangeText={(val) => setName(val)}/>
-			<TextInput style={styles.input} placeholder= 'Password'
-			onChangeText={(val) => setPassword(val)}/>
-			<Text style={styles.warning}>{message}</Text>
-			<Button title='Verify Account' onPress={fetchtoken} />
+		<ImageBackground source={require('../assets/background.png')} style={styles.image} >
+			<View style={styles.container}>  			 
+				<Text style={styles.boldText}>Sync Account</Text>
+				<View style={styles.buttonContainer}>
+					<TextInput style={styles.input} placeholder='User Name' onChangeText={(val) => setName(val)}/>
+					<TextInput style={styles.input} placeholder= 'Password' onChangeText={(val) => setPassword(val)}/>
+					<Text style={styles.warning}>{message}</Text>
+					<Button title='Verify Account' onPress={fetchtoken} />
+				</View>
 			</View>
-		</View>
-
-		);
+		</ImageBackground>
+	);
 }
+	const styles = StyleSheet.create({
+		container:{
+			alignItems: 'center',
+			justifyContent:'center',
+			padding:20,
+			color: 'white'
+		},
+		boldText:{
+			fontWeight: 'bold',
+		},
+		buttonContainer:{
+			marginTop:20
+		},
+		input:{ 
+			borderColor: '#777',
+			padding: 20,
+			margin:10
 
-const styles = StyleSheet.create({
-	container:{
-		alignItems: 'center',
-		justifyContent:'center',
-		padding:20,
-		backgroundColor: 'lavender',
-		
 
+		},
+		warning:{
+			color: 'red',
+			margin:10
+		},
+		image: {
+	    flex: 1,
+	    justifyContent: "center",
+	    weight:'100%',
+	    heiht:'100%'
 	},
-	boldText:{
-		fontWeight: 'bold',
-	},
-	buttonContainer:{
-		marginTop:20
-	},
-	input:{ 
-		borderColor: '#777',
-		padding: 20,
-		margin:10
-
-
-	},
-	warning:{
-		color: 'red',
-		margin:10
-	}
-
 })
